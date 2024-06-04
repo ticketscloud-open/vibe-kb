e285ac0dffaa6a18c738144ebefe643729e21865
Kirill Klenov <horneds@gmail.com>
2024-06-04 22:43:11 +0300

    feat: wip

--- content/Obsidian.md
+++ content/Obsidian.md
@@ -1,3 +1,10 @@
-*[Obsidian](https://obsidian.md) открытое приложение для ведения заметок.*
+## Загрузка и установка
 
-- Скачать можно [по ссылке](https://obsidian.md/download)
+- Скачать можно с официального сайта [по ссылке](https://obsidian.md/download)
+
+![](https://www.youtube.com/watch?v=CKRgUveNZx8&list=PLeDR6lYFEHWEUxwSA8OplPLvk50DCVraH)
+## Работа с Obsidian
+
+![](https://www.youtube.com/watch?v=ye9YMLQ8hY0&list=PLeDR6lYFEHWEUxwSA8OplPLvk50DCVraH)
+
+![](https://www.youtube.com/watch?v=lOISSwHM7e0&list=PLeDR6lYFEHWEUxwSA8OplPLvk50DCVraH)
--- quartz.layout.ts
+++ quartz.layout.ts
@@ -7,8 +7,8 @@ export const sharedPageComponents: SharedLayout = {
   header: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      GitHub: "https://github.com/ticketscloud/vibe-kb",
+      Slack: "https://ticketscloud.slack.com/archives/CKBMM7U8Z",
     },
   }),
 }
@@ -29,9 +29,9 @@ export const defaultContentPageLayout: PageLayout = {
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
-    Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
+    Component.Graph(),
   ],
 }
 
