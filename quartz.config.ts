36ce3f08c8cacb2b3431d4dd4426ff1459592c5b
Kirill Klenov <horneds@gmail.com>
2025-06-19 17:33:54 +0700

    fix: update baseUrl configuration 🛠️

--- quartz.config.ts
+++ quartz.config.ts
@@ -13,7 +13,7 @@ const config: QuartzConfig = {
     enablePopovers: true,
     analytics: null,
     locale: "ru-RU",
-    baseUrl: "https://kb.vibeapp.ru",
+    baseUrl: "kb.vibeapp.ru",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
